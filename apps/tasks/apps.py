ae72a841993beba2a30302ba410e76f7b242e2f7
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-06-26 11:54:57 +0600

    feat: add docstrings

--- apps/tasks/apps.py
+++ apps/tasks/apps.py
@@ -1,6 +1,8 @@
+''' Application configuration module '''
 from django.apps import AppConfig
 
 
 class TasksConfig(AppConfig):
+    ''' Application configuration class '''
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tasks'
+    name = 'apps.tasks'
