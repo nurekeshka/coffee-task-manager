1a463d2924539bb2244e1f56c9e63f24f190a03a
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-06-27 12:25:35 +0600

    feat: add user model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,11 @@
+''' This file contains account models
+    related to authentication processes
+'''
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
