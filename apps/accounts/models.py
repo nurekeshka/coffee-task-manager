7cd15acd6d2458e1301eb4a34e4b5490e15cf721
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-06-27 12:28:38 +0600

    feat: add organization model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -9,3 +9,19 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'user'
         verbose_name_plural = 'users'
+
+
+class Organization(models.Model):
+    ''' This model represents organization in database. '''
+    name: str = models.CharField(max_length=255, verbose_name='name')
+    creator: User = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True,
+        null=True, verbose_name='creator')
+
+    class Meta:
+        ''' Meta data for organization models. '''
+        verbose_name = 'organization'
+        verbose_name_plural = 'organizations'
+
+    def __str__(self) -> str:
+        return str(self.name)
