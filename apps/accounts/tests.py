97a1631985e35372ba61d9cb4e15d92e51d2debe
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-06-27 12:58:04 +0600

    feat: add user creation testcase

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+''' This file contains tests for the accounts application. '''
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+from django.urls import reverse
+
+from apps.accounts.models import User, Organization
+
+
+class UserAPITestCase(APITestCase):
+    username = 'admin'
+    password = 'root'
+
+    email = 'admin@example.com'
+
+    first_name = 'John'
+    last_name = 'Doe'
+
+    def test__creation(self):
+        link = reverse('users')
+
+        response = self.client.post(link, {
+            'username': self.username,
+            'password': self.password,
+        })
+
+        token = Token.objects.get(user__username=self.username)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json()['authentication-token'], token.key)
