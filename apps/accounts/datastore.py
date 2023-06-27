cf51fc67220a5804976583efe5ab5f49adcaab58
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-06-27 14:56:23 +0600

    fix: datastore methods are classmethods

--- apps/accounts/datastore.py
+++ apps/accounts/datastore.py
@@ -9,25 +9,28 @@ from apps.accounts.entities import User
 class UserDBRepository:
     ''' This class represents user datastore repositories. '''
 
-    def get_find_by_id(self, identity: int) -> List[User]:
+    @classmethod
+    def get_find_by_id(cls, identity: int) -> List[User]:
         ''' Find a user by id from the database and return entity. '''
         try:
             orm_user = UserORM.objects.get(id=identity)
         except UserORM.DoesNotExist as exception:
             raise EntityDoesNotExist from exception
 
-        return self.decode_orm_user(orm_user)
+        return cls.decode_orm_user(orm_user)
 
-    def get_find(self) -> List[User]:
+    @classmethod
+    def get_find(cls) -> List[User]:
         ''' Get all users from the database and return as entities. '''
         try:
             orm_users = UserORM.objects.all()
         except UserORM.DoesNotExist as exception:
             raise EntityDoesNotExist from exception
 
-        return self.decode_orm_users(orm_users)
+        return cls.decode_orm_users(orm_users)
 
-    def decode_orm_user(self, orm_user_queryset: UserORM) -> List[User]:
+    @classmethod
+    def decode_orm_user(cls, orm_user_queryset: UserORM) -> List[User]:
         ''' Decode an ORM user into a user entity. '''
         user = User()
 
@@ -45,12 +48,13 @@ class UserDBRepository:
 
         return [user_entity]
 
-    def decode_orm_users(self, orm_user_queryset: List[UserORM]) -> List[User]:
+    @classmethod
+    def decode_orm_users(cls, orm_user_queryset: List[UserORM]) -> List[User]:
         ''' Decode multiple users into user entities. '''
         users_list = list()
 
         for orm_user in orm_user_queryset:
-            user = self.decode_orm_user(orm_user)
+            user = cls.decode_orm_user(orm_user)
             users_list.extend(user)
 
         return users_list
