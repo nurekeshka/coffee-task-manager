9df6f9e0f9ffe0e4240fdb587996b73a7044b19b
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-06-27 12:50:42 +0600

    feat: add user admin class

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apps.accounts import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'username', 'email', 'first_name', 'last_name')
+    fields = ('username', 'first_name', 'last_name', 'email',
+              'is_staff', 'is_active')
